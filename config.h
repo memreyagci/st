6212b86196efeab175b69bbf6098c382f7e94d79
memreyagci <meydeveloper@tutanota.com>
2020-12-26 20:50:36 +0100

    alpha value is set to 0.8

--- config.h
+++ config.h
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.9;
+float alpha = 0.8;
 
 #include "/home/musa/.cache/wal/colors-wal-st.h"
 ///* Terminal colors (16 first used in escape sequence) */
